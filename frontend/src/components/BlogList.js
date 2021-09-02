5d420fe38d21f0f27afb5c971c7d9eb9337b0bea
System Administrator <root@Pavels-MacBook-Pro.local>
2021-09-02 04:03:28 +0300

    Added a BlogList component

---
+++ frontend/src/components/BlogList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogList = () => {
+  return (
+    <div style={{ margin: 10, padding: 10 }}>
+      <h1>Blog List</h1>
+    </div>
+  );
+}
+
+export default BlogList
